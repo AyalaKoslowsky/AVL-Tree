f28af9706194158b95b99a75d8ac016804c432f3
AyalaKoslowsky <105251129+AyalaKoslowsky@users.noreply.github.com>
2022-08-11 16:18:25 +0300

    Update AVLTree.java

--- AVLTree.java
+++ AVLTree.java
@@ -1015,38 +1015,4 @@ public class AVLTree {
 	public IAVLNode getRoot() {
 		return this.root;
 	}
-	
-	
-
-	/**
-	 * public interface IAVLNode ! Do not delete or modify this - otherwise all
-	 * tests will fail !
-	 */
-	public interface IAVLNode {
-		public int getKey(); // Returns node's key (for virtual node return -1).
-
-		public String getValue(); // Returns node's value [info], for virtual node returns null.
-
-		public void setLeft(IAVLNode node); // Sets left child.
-
-		public IAVLNode getLeft(); // Returns left child, if there is no left child returns null.
-
-		public void setRight(IAVLNode node); // Sets right child.
-
-		public IAVLNode getRight(); // Returns right child, if there is no right child return null.
-
-		public void setParent(IAVLNode node); // Sets parent.
-
-		public IAVLNode getParent(); // Returns the parent, if there is no parent return null.
-
-		public boolean isRealNode(); // Returns True if this is a non-virtual AVL node.
-
-		public void setHeight(int height); // Sets the height of the node.
-
-		public int getHeight(); // Returns the height of the node (-1 for virtual nodes).
-
-		public int getSize(); // Returns the size of the nodes-subTree (including him)
-
-		public void setSize(int newSize); // Sets the size of the node
-	}
 }
